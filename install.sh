cef1530be559a5db815f51e6070aacf6ee8f5c80
nghdavid <35591407+nghdavid@users.noreply.github.com>
2021-09-07 17:34:05 +0800

    Pre-requiste software

---
+++ install.sh
@@ -0,0 +1,12 @@
+conda create -n caiman  # caiman here refers to the name of the environment (you can pick any name you want)
+conda activate caiman
+conda install caiman -c conda-forge
+
+
+
+caimanmanager.py install
+#Once CaImAn is installed, you may want to get a working directory with code samples and datasets; pip installed a caimanmanager.py command that manages this.
+
+pip install scanimage-tiff-reader
+
+
