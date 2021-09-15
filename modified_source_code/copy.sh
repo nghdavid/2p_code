1b3ed47bc6e15299cab2fe370586ecd2f75118cc
nghdavid <35591407+nghdavid@users.noreply.github.com>
2021-09-15 17:21:44 +0800

    A code to replace caiman original code

---
+++ modified_source_code/copy.sh
@@ -0,0 +1,4 @@
+#Remember to change the user name
+cp /home/david/caiman_data/2p_code/modified_source_code/estimates.py /home/david/anaconda3/envs/caiman/lib/python3.9/site-packages/caiman/source_extraction/cnmf/estimates.py 
+cp /home/david/caiman_data/2p_code/modified_source_code/visualization.py /home/david/anaconda3/envs/caiman/lib/python3.9/site-packages/caiman/utils/visualization.py
+
